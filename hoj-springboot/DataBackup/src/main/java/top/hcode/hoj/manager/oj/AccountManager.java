updahn/NYOJ-3.0

Add UserHome Contests-ranking-changes

hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/manager/oj/AccountManager.java
@@ -14,15 +14,19 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import top.hcode.hoj.common.exception.StatusFailException;
 import top.hcode.hoj.common.exception.StatusSystemErrorException;
+import top.hcode.hoj.dao.contest.ContestEntityService;
+import top.hcode.hoj.dao.contest.ContestRecordEntityService;
 import top.hcode.hoj.dao.problem.ProblemEntityService;
 import top.hcode.hoj.dao.user.*;
 import top.hcode.hoj.manager.email.EmailManager;
 import top.hcode.hoj.pojo.dto.ChangeEmailDTO;
 import top.hcode.hoj.pojo.dto.ChangePasswordDTO;
 import top.hcode.hoj.pojo.dto.CheckUsernameOrEmailDTO;
+import top.hcode.hoj.pojo.entity.contest.ContestRecord;
 import top.hcode.hoj.pojo.entity.judge.Judge;
 import top.hcode.hoj.pojo.entity.problem.Problem;
 import top.hcode.hoj.pojo.entity.user.Role;
+import top.hcode.hoj.pojo.entity.contest.Contest;
 import top.hcode.hoj.pojo.entity.user.Session;
 import top.hcode.hoj.pojo.entity.user.UserAcproblem;
 import top.hcode.hoj.pojo.entity.user.UserInfo;
@@ -53,7 +57,7 @@ public class AccountManager {
 
     @Autowired
     private UserPreferencesEntityService userPreferencesEntityService;
-    
+
     @Autowired
     private UserRoleEntityService userRoleEntityService;
 
@@ -64,7 +68,13 @@ public class AccountManager {
     private UserAcproblemEntityService userAcproblemEntityService;
 
     @Autowired
+    private ContestRecordEntityService contestRecordEntityService;
+
+    @Autowired
     private ProblemEntityService problemEntityService;
+
+    @Autowired
+    private ContestEntityService contestEntityService;
 
     @Autowired
     private SessionEntityService sessionEntityService;
@@ -74,6 +84,9 @@ public class AccountManager {
 
     @Autowired
     private EmailManager emailManager;
+
+    @Autowired
+    private ContestRankManager contestRankManager;
 
     /**
      * @MethodName checkUsernameOrEmail
@@ -170,6 +183,13 @@ public class AccountManager {
             disPlayIdList = problems.stream().map(Problem::getProblemId).collect(Collectors.toList());
         }
         userHomeInfo.setSolvedList(disPlayIdList);
+
+        UserContestsRankingVO userContestsRankingVo = getUserContestsRanking(uid, username);
+        // 获取用户参加的比赛
+        userHomeInfo.setContestPidList((List<Long>) userContestsRankingVo.getSolvedList());
+        // 获取用户参加比赛的榜单
+        userHomeInfo.setDataList((List<HashMap<String, Object>>) userContestsRankingVo.getDataList());
+
         QueryWrapper<Session> sessionQueryWrapper = new QueryWrapper<>();
         sessionQueryWrapper.eq("uid", userHomeInfo.getUid())
                 .orderByDesc("gmt_create")
@@ -227,6 +247,57 @@ public class AccountManager {
         }
         userCalendarHeatmapVo.setDataList(dataList);
         return userCalendarHeatmapVo;
+    }
+
+    /**
+     * @param uid
+     * @return
+     * @Description 获取用户最近一年参加的比赛
+     */
+    public List<Contest> getUserContests(String uid) throws StatusFailException {
+
+        // 获取已经结束, 不包含赛后提交，可见的ACM比赛的状态, 比赛结束后开榜的比赛
+        QueryWrapper<Contest> contestQueryWrapper = new QueryWrapper<>();
+        contestQueryWrapper
+                .select("id")
+                .eq("status", 1) // 获取已经结束
+                .eq("visible", 1) // 可见
+                .eq("auto_real_rank", 1) // 比赛结束后开榜的比赛
+                .eq("type", 0) // ACM比赛
+                .isNull("gid") // 不为组队的
+                .orderByAsc("end_time"); // 参加的比赛按照结束时间升序排序
+
+        List<Long> pidList = contestEntityService.list(contestQueryWrapper).stream().map(Contest::getId)
+                .collect(Collectors.toList());
+        QueryWrapper<ContestRecord> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("distinct cid")
+                .eq("uid", uid)
+                .in("cid", pidList);
+        List<ContestRecord> userContestList = contestRecordEntityService.list(queryWrapper);
+
+        List<Contest> contestList = new ArrayList<>();
+        if (CollectionUtils.isEmpty(userContestList)) {
+            return contestList;
+        }
+
+        contestList = userContestList.stream()
+                .map(contestRecord -> contestEntityService.getById(contestRecord.getCid()))
+                .collect(Collectors.toList());
+
+        return contestList;
+    }
+
+    /**
+     * @param uid
+     * @param username
+     * @return
+     * @Description 获取用户的比赛名次变化图
+     */
+    public UserContestsRankingVO getUserContestsRanking(String uid, String username) throws StatusFailException {
+
+        List<Contest> contestList = getUserContests(uid);
+
+        return contestRankManager.getRecentYearContestsRanking(contestList, uid, username);
     }
 
     /**

hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/manager/oj/ContestRankManager.java
@@ -2,17 +2,27 @@ package top.hcode.hoj.manager.oj;
 
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import top.hcode.hoj.common.exception.StatusFailException;
 import top.hcode.hoj.pojo.entity.contest.Contest;
 import top.hcode.hoj.pojo.vo.ACMContestRankVO;
 import top.hcode.hoj.pojo.vo.OIContestRankVO;
+import top.hcode.hoj.pojo.vo.UserContestsRankingVO;
+import top.hcode.hoj.shiro.AccountProfile;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
+
+import java.util.*;
+import java.util.regex.*;
 
 /**
  * @Author: Himit_ZH
@@ -118,6 +128,39 @@ public class ContestRankManager {
 
         // 计算好排行榜，然后进行分页
         return getPagingRankList(orderResultList, currentPage, limit);
+    }
+
+    /**
+     * @param isOpenSealRank              是否封榜
+     * @param removeStar                  是否移除打星队伍
+     * @param currentUserId               当前用户id
+     * @param concernedList               关联比赛的id列表
+     * @param contest                     比赛信息
+     * @param isContainsAfterContestJudge 是否包含比赛结束后的提交
+     * @param selectedTime                比赛跳转榜单的时间
+     * @desc 获取ACM比赛排行榜
+     */
+    public List<ACMContestRankVO> getContestACMRankList(
+            Boolean isOpenSealRank,
+            Boolean removeStar,
+            String currentUserId,
+            List<String> concernedList,
+            List<Integer> externalCidList,
+            Contest contest,
+            Boolean isContainsAfterContestJudge,
+            Long selectedTime) {
+
+        // 进行排序计算
+        List<ACMContestRankVO> orderResultList = contestCalculateRankManager.calcACMRank(
+                isOpenSealRank,
+                removeStar,
+                contest,
+                currentUserId,
+                concernedList,
+                externalCidList,
+                isContainsAfterContestJudge);
+
+        return orderResultList;
     }
 
     /**
@@ -231,6 +274,89 @@ public class ContestRankManager {
                     .collect(Collectors.toList());
         }
         return getPagingRankList(oiContestRankVOList, currentPage, limit);
+    }
+
+    /**
+     * @param uid
+     * @param username
+     * @return
+     * @Description 获取用户的比赛名次变化图
+     */
+    public UserContestsRankingVO getRecentYearContestsRanking(
+            List<Contest> contestList,
+            String uid,
+            String username) throws StatusFailException {
+
+        AccountProfile userRolesVo = (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+        if (StringUtils.isEmpty(uid) && StringUtils.isEmpty(username)) {
+            if (userRolesVo != null) {
+                uid = userRolesVo.getUid();
+            } else {
+                throw new StatusFailException("请求参数错误：uid和username不能都为空！");
+            }
+        }
+
+        UserContestsRankingVO userContestsRankingVO = new UserContestsRankingVO();
+        userContestsRankingVO.setEndDate(DateUtil.format(new Date(), "yyyy-MM-dd"));
+
+        if (CollectionUtils.isEmpty(contestList)) {
+            userContestsRankingVO.setDataList(new ArrayList<>());
+            userContestsRankingVO.setSolvedList(new ArrayList<>());
+            return userContestsRankingVO;
+        }
+
+        List<HashMap<String, Object>> dataList = new ArrayList<>();
+        List<Long> contestPids = new ArrayList<>();
+        for (Contest contest : contestList) {
+
+            List<ACMContestRankVO> orderResultList = getContestACMRankList(
+                    false,
+                    true,
+                    uid,
+                    null,
+                    null,
+                    contest,
+                    false,
+                    null);
+            String keyword = username;
+
+            if (StrUtil.isNotBlank(keyword)) {
+                String finalKeyword = keyword.trim().toLowerCase();
+                orderResultList = orderResultList.stream()
+                        .filter(rankVo -> filterBySchoolORRankShowName(finalKeyword,
+                                rankVo.getSchool(),
+                                getUserRankShowName(contest.getRankShowName(),
+                                        rankVo.getUsername(),
+                                        rankVo.getRealname(),
+                                        rankVo.getNickname())))
+                        .collect(Collectors.toList());
+            }
+
+            if (orderResultList.size() > 0) {
+                String user_uid = orderResultList.get(0).getUid();
+                if (user_uid.equals(uid)) {
+                    contestPids.add(contest.getId());
+                    Integer rank = orderResultList.get(0).getRank();
+                    Date startTime = contest.getStartTime();
+                    String dateStr = DateUtil.format(startTime, "yyyy-MM-dd HH:mm");
+                    HashMap<String, Object> tmp = new HashMap<>(4);
+                    tmp.put("date", dateStr);
+                    tmp.put("rank", rank);
+                    tmp.put("cid", contest.getId());
+                    tmp.put("title", contest.getTitle());
+                    dataList.add(tmp);
+                }
+            }
+        }
+
+        if (CollectionUtils.isEmpty(dataList)) {
+            userContestsRankingVO.setSolvedList(new ArrayList<>());
+            userContestsRankingVO.setDataList(new ArrayList<>());
+            return userContestsRankingVO;
+        }
+        userContestsRankingVO.setSolvedList(contestPids);
+        userContestsRankingVO.setDataList(dataList);
+        return userContestsRankingVO;
     }
 
     private <T> Page<T> getPagingRankList(List<T> rankList, int currentPage, int limit) {

IDYMI/HOJ

hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/manager/oj/AccountManager.java
@@ -14,15 +14,19 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import top.hcode.hoj.common.exception.StatusFailException;
 import top.hcode.hoj.common.exception.StatusSystemErrorException;
+import top.hcode.hoj.dao.contest.ContestEntityService;
+import top.hcode.hoj.dao.contest.ContestRecordEntityService;
 import top.hcode.hoj.dao.problem.ProblemEntityService;
 import top.hcode.hoj.dao.user.*;
 import top.hcode.hoj.manager.email.EmailManager;
 import top.hcode.hoj.pojo.dto.ChangeEmailDTO;
 import top.hcode.hoj.pojo.dto.ChangePasswordDTO;
 import top.hcode.hoj.pojo.dto.CheckUsernameOrEmailDTO;
+import top.hcode.hoj.pojo.entity.contest.ContestRecord;
 import top.hcode.hoj.pojo.entity.judge.Judge;
 import top.hcode.hoj.pojo.entity.problem.Problem;
 import top.hcode.hoj.pojo.entity.user.Role;
+import top.hcode.hoj.pojo.entity.contest.Contest;
 import top.hcode.hoj.pojo.entity.user.Session;
 import top.hcode.hoj.pojo.entity.user.UserAcproblem;
 import top.hcode.hoj.pojo.entity.user.UserInfo;
@@ -34,6 +38,7 @@ import top.hcode.hoj.utils.RedisUtils;
 import top.hcode.hoj.validator.CommonValidator;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -53,7 +58,7 @@ public class AccountManager {
 
     @Autowired
     private UserPreferencesEntityService userPreferencesEntityService;
-    
+
     @Autowired
     private UserRoleEntityService userRoleEntityService;
 
@@ -64,7 +69,13 @@ public class AccountManager {
     private UserAcproblemEntityService userAcproblemEntityService;
 
     @Autowired
+    private ContestRecordEntityService contestRecordEntityService;
+
+    @Autowired
     private ProblemEntityService problemEntityService;
+
+    @Autowired
+    private ContestEntityService contestEntityService;
 
     @Autowired
     private SessionEntityService sessionEntityService;
@@ -74,6 +85,9 @@ public class AccountManager {
 
     @Autowired
     private EmailManager emailManager;
+
+    @Autowired
+    private ContestRankManager contestRankManager;
 
     /**
      * @MethodName checkUsernameOrEmail
@@ -139,6 +153,7 @@ public class AccountManager {
         if (StringUtils.isEmpty(uid) && StringUtils.isEmpty(username)) {
             if (userRolesVo != null) {
                 uid = userRolesVo.getUid();
+                username = userRolesVo.getUsername();
             } else {
                 throw new StatusFailException("请求参数错误：uid和username不能都为空！");
             }
@@ -148,6 +163,7 @@ public class AccountManager {
         if (userHomeInfo == null) {
             throw new StatusFailException("用户不存在");
         }
+
         QueryWrapper<UserAcproblem> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("uid", userHomeInfo.getUid())
                 .select("distinct pid", "submit_id")
@@ -170,6 +186,14 @@ public class AccountManager {
             disPlayIdList = problems.stream().map(Problem::getProblemId).collect(Collectors.toList());
         }
         userHomeInfo.setSolvedList(disPlayIdList);
+
+        UserContestsRankingVO userContestsRankingVo = getUserContestsRanking(uid, username);
+
+        // 获取用户参加的比赛
+        userHomeInfo.setContestPidList((List<Long>) userContestsRankingVo.getSolvedList());
+        // 获取用户参加比赛的榜单
+        userHomeInfo.setDataList((List<HashMap<String, Object>>) userContestsRankingVo.getDataList());
+
         QueryWrapper<Session> sessionQueryWrapper = new QueryWrapper<>();
         sessionQueryWrapper.eq("uid", userHomeInfo.getUid())
                 .orderByDesc("gmt_create")
@@ -179,6 +203,7 @@ public class AccountManager {
         if (recentSession != null) {
             userHomeInfo.setRecentLoginTime(recentSession.getGmtCreate());
         }
+
         return userHomeInfo;
     }
 
@@ -227,6 +252,57 @@ public class AccountManager {
         }
         userCalendarHeatmapVo.setDataList(dataList);
         return userCalendarHeatmapVo;
+    }
+
+    /**
+     * @param uid
+     * @return
+     * @Description 获取用户参加的比赛
+     */
+    public List<Contest> getUserContests(String uid) throws StatusFailException {
+
+        // 获取已经结束, 不包含赛后提交，可见的ACM比赛的状态, 比赛结束后开榜的比赛
+        QueryWrapper<Contest> contestQueryWrapper = new QueryWrapper<>();
+        contestQueryWrapper
+                .select("id")
+                .eq("status", 1) // 获取已经结束
+                .eq("visible", 1) // 可见
+                .eq("auto_real_rank", 1) // 比赛结束后开榜的比赛
+                .eq("type", 0) // ACM比赛
+                .isNull("gid") // 不为团队的
+                .orderByAsc("end_time"); // 参加的比赛按照结束时间升序排序
+
+        List<Long> pidList = contestEntityService.list(contestQueryWrapper).stream().map(Contest::getId)
+                .collect(Collectors.toList());
+        QueryWrapper<ContestRecord> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("distinct cid")
+                .eq("uid", uid)
+                .in("cid", pidList);
+        List<ContestRecord> userContestList = contestRecordEntityService.list(queryWrapper);
+
+        List<Contest> contestList = new ArrayList<>();
+        if (CollectionUtils.isEmpty(userContestList)) {
+            return contestList;
+        }
+
+        contestList = userContestList.stream()
+                .map(contestRecord -> contestEntityService.getById(contestRecord.getCid()))
+                .collect(Collectors.toList());
+
+        return contestList;
+    }
+
+    /**
+     * @param uid
+     * @param username
+     * @return
+     * @Description 获取用户的比赛名次变化图
+     */
+    public UserContestsRankingVO getUserContestsRanking(String uid, String username) throws StatusFailException {
+
+        List<Contest> contestList = getUserContests(uid);
+
+        return contestRankManager.getContestsRanking(contestList, uid, username);
     }
 
     /**

hoj-springboot/DataBackup/src/main/java/top/hcode/hoj/manager/oj/ContestRankManager.java
@@ -2,17 +2,27 @@ package top.hcode.hoj.manager.oj;
 
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import top.hcode.hoj.common.exception.StatusFailException;
 import top.hcode.hoj.pojo.entity.contest.Contest;
 import top.hcode.hoj.pojo.vo.ACMContestRankVO;
 import top.hcode.hoj.pojo.vo.OIContestRankVO;
+import top.hcode.hoj.pojo.vo.UserContestsRankingVO;
+import top.hcode.hoj.shiro.AccountProfile;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
+
+import java.util.*;
+import java.util.regex.*;
 
 /**
  * @Author: Himit_ZH
@@ -118,6 +128,39 @@ public class ContestRankManager {
 
         // 计算好排行榜，然后进行分页
         return getPagingRankList(orderResultList, currentPage, limit);
+    }
+
+    /**
+     * @param isOpenSealRank              是否封榜
+     * @param removeStar                  是否移除打星队伍
+     * @param currentUserId               当前用户id
+     * @param concernedList               关联比赛的id列表
+     * @param contest                     比赛信息
+     * @param isContainsAfterContestJudge 是否包含比赛结束后的提交
+     * @param selectedTime                比赛跳转榜单的时间
+     * @desc 获取ACM比赛排行榜
+     */
+    public List<ACMContestRankVO> getContestACMRankList(
+            Boolean isOpenSealRank,
+            Boolean removeStar,
+            String currentUserId,
+            List<String> concernedList,
+            List<Integer> externalCidList,
+            Contest contest,
+            Boolean isContainsAfterContestJudge,
+            Long selectedTime) {
+
+        // 进行排序计算
+        List<ACMContestRankVO> orderResultList = contestCalculateRankManager.calcACMRank(
+                isOpenSealRank,
+                removeStar,
+                contest,
+                currentUserId,
+                concernedList,
+                externalCidList,
+                isContainsAfterContestJudge);
+
+        return orderResultList;
     }
 
     /**
@@ -231,6 +274,91 @@ public class ContestRankManager {
                     .collect(Collectors.toList());
         }
         return getPagingRankList(oiContestRankVOList, currentPage, limit);
+    }
+
+    /**
+     * @param uid
+     * @param username
+     * @return
+     * @Description 获取用户的比赛名次变化图
+     */
+    public UserContestsRankingVO getContestsRanking(
+            List<Contest> contestList,
+            String uid,
+            String username) throws StatusFailException {
+
+        AccountProfile userRolesVo = (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+        if (StringUtils.isEmpty(uid) && StringUtils.isEmpty(username)) {
+            if (userRolesVo != null) {
+                uid = userRolesVo.getUid();
+            } else {
+                throw new StatusFailException("请求参数错误：uid和username不能都为空！");
+            }
+        }
+
+        UserContestsRankingVO userContestsRankingVO = new UserContestsRankingVO();
+        userContestsRankingVO.setEndDate(DateUtil.format(new Date(), "yyyy-MM-dd"));
+
+        if (CollectionUtils.isEmpty(contestList)) {
+            userContestsRankingVO.setDataList(new ArrayList<>());
+            userContestsRankingVO.setSolvedList(new ArrayList<>());
+            return userContestsRankingVO;
+        }
+
+        List<HashMap<String, Object>> dataList = new ArrayList<>();
+        List<Long> contestPids = new ArrayList<>();
+        String finalKeyword = username.trim().toLowerCase();
+        // 将 uid 声明为 final
+        final String finalUid = uid;
+
+        // 并行流处理
+        contestList.parallelStream().forEach(contest -> {
+            List<ACMContestRankVO> orderResultList = getContestACMRankList(
+                    false,
+                    true,
+                    null,
+                    null,
+                    null,
+                    contest,
+                    false,
+                    null);
+
+            orderResultList = orderResultList.stream()
+                    .filter(rankVo -> filterBySchoolORRankShowName(finalKeyword,
+                            rankVo.getSchool(),
+                            getUserRankShowName(contest.getRankShowName(),
+                                    rankVo.getUsername(),
+                                    rankVo.getRealname(),
+                                    rankVo.getNickname())))
+                    .collect(Collectors.toList());
+
+            if (orderResultList.size() > 0) {
+                String user_uid = orderResultList.get(0).getUid();
+                if (user_uid.equals(finalUid)) {
+                    contestPids.add(contest.getId());
+                    Integer rank = orderResultList.get(0).getRank();
+                    Date startTime = contest.getStartTime();
+                    String dateStr = DateUtil.format(startTime, "yyyy-MM-dd HH:mm");
+                    HashMap<String, Object> tmp = new HashMap<>(4);
+                    tmp.put("date", dateStr);
+                    tmp.put("rank", rank);
+                    tmp.put("cid", contest.getId());
+                    tmp.put("title", contest.getTitle());
+                    synchronized (dataList) {
+                        dataList.add(tmp);
+                    }
+                }
+            }
+        });
+
+        if (CollectionUtils.isEmpty(dataList)) {
+            userContestsRankingVO.setSolvedList(new ArrayList<>());
+            userContestsRankingVO.setDataList(new ArrayList<>());
+            return userContestsRankingVO;
+        }
+        userContestsRankingVO.setSolvedList(contestPids);
+        userContestsRankingVO.setDataList(dataList);
+        return userContestsRankingVO;
     }
 
     private <T> Page<T> getPagingRankList(List<T> rankList, int currentPage, int limit) {